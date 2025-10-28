306656e033051fa1365d1396ab022b6ca5b6292b
benduran <stratodyne@gmail.com>
2025-10-28 09:29:02 +0000

    chore: added typings file

---
+++ types.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'eslint-config-react-yas' {
+  export const standalone: any[];
+  export const respectPrettierConfig: any[];
+}
